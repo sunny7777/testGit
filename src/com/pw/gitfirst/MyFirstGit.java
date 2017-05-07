d30771f87d95bed5f25bab06e041ccdc42eb21d3
lipingwei <idpingwei@163.com>
2017-05-07 10:17:38 +0800

    this my first commit

---
+++ src/com/pw/gitfirst/MyFirstGit.java
@@ -0,0 +1,10 @@
+package com.pw.gitfirst;
+
+/**
+ * Created by Sunny on 2017/5/7.
+ */
+public class MyFirstGit {
+    public static void main(String[] args){
+         System.out.println("这是我的第一个git程序");
+    }
+}
